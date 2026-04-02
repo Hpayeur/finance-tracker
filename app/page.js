e014a96cd167d23f62bf41996bb49aa6e7132be5
Hpayeur <hailpaye017@gmail.com>
2026-04-01 19:22:43 -0600

    video 7

--- app/page.js
+++ app/page.js
@@ -1,10 +1,23 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { currencyFormatter } from "@/app/lib/utils";
 import ExpenseCategoryItem from "@/app/components/ExpenseCategoryItem";
 import Modal from "@/app/components/Modal";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+
+//firebase
+import { db } from "@/app/lib/firebase/index";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+
+// Icons
+import { FaRegTrashAlt } from "react-icons/fa";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -42,12 +55,81 @@ const DUMMY_DATA = [
 ];
 
 export default function Home() {
-  const [modalIsOpen, setModalIsopen] = useState(true);
+  const [income, setIncome] = useState([]);
+  const [showAddIncomeModal, setShowAddIncomeModal] = useState(false);
+  const amountRef = useRef();
+  const descriptionRef = useRef();
+
+  useEffect(() => {
+    const getIncomeData = async () => {
+      const collectionRef = collection(db, "income");
+      const docsSnap = await getDocs(collectionRef);
+
+      const data = docsSnap.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+          createdAt: new Date(doc.data().createdAt.toMillis()),
+        };
+      });
+      setIncome(data);
+    };
+    getIncomeData();
+  }, []);
 
   return (
     <>
-      <Modal show={modalIsOpen} onClose={setModalIsopen}>
-        <h3>Hello, Let's Get Started! On your journey to financial freedom!</h3>
+      {/* Add Income Modal */}
+      <Modal show={showAddIncomeModal} onClose={setShowAddIncomeModal}>
+        <form onSubmit={addIncomeHandler} className="flex flex-col gap-4">
+          <div className="input-group">
+            <label htmlfor="amount">Income Amount</label>
+            <input
+              type="number"
+              ref={amountRef}
+              min={0.01}
+              step={0.01}
+              placeholder="Enter income amount"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlfor="description">Description</label>
+            <input
+              name="description"
+              ref={descriptionRef}
+              type="text"
+              placeholder="Enter description"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Add Entry
+          </button>
+        </form>
+        <div className="flex flex-col gap-4 mt-6">
+          <h3 className="text-2xl font-bold">Income History</h3>
+          {income.map((i) => {
+            return (
+              <div className="flex justify-between items-center" key={i.id}>
+                <div>
+                  <p className="font-semibold">{i.description}</p>
+                  <small className="text-xs">{i.createdAt.toISOString()}</small>
+                </div>
+                <p className="flex items-center gap-2">
+                  {currencyFormatter(i.amount)}
+                  <button
+                    onClick={() => {
+                      deleteIncomeEntryHandler(i.id);
+                    }}
+                  >
+                    <FaRegTrashAlt />
+                  </button>
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </Modal>
       <main className="container max-w-2xl px-6 mx-auto">
         <section className="py-3">
@@ -55,13 +137,15 @@ export default function Home() {
           <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
         </section>
         <section className="flex items-center gap-2 py-3">
+          <button className="btn btn-primary">+ Expenses</button>
           <button
-            className="btn btn-primary"
-            onClick={() => setModalIsopen(true)}
+            onClick={() => {
+              setShowAddIncomeModal(true);
+            }}
+            className="btn btn-primary-outline"
           >
-            + Expenses
+            + Income
           </button>
-          <button className="btn btn-primary-outline">+ Income</button>
         </section>
 
         {/* Expenses */}
@@ -71,6 +155,7 @@ export default function Home() {
             {DUMMY_DATA.map((expense) => {
               return (
                 <ExpenseCategoryItem
+                  key={expense.id}
                   color={expense.color}
                   title={expense.title}
                   total={expense.total}
